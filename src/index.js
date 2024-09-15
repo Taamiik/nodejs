ec56b23b6eb0d948377f233a0db7a9f5ae8e7703
Томаев Тамик Казбекович <ttomaev@nota.tech>
2024-09-15 13:07:15 +0300

    3 commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import express from "express";
+import { sum } from "./about";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (_request, response) => {
   response.send("Hello world");
+  console.log(sum(1, 2));
 });
 
 app.listen(PORT, () => {
